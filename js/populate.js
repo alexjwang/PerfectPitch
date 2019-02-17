7a12543d1cc2b341377b996a1018ac5a262e0f31
Alex Wang <alexwang3150@gmail.com>
2019-02-17 11:26:59 -0500

    editing view pages and making it look like an actual forum

--- js/populate.js
+++ js/populate.js
@@ -14,11 +14,11 @@ xmlhttp.onreadystatechange = function() {
                   + proj
                   + "</a></h3><p>"
                   + desc
-                  + "</div> <div class = 'container-right'> <h3> </h3> <span>Upvotes: " 
+                  + "</div> <div class = 'container-right'> <h3> </h3> <p>Upvotes: " 
                   + votes 
-                  + "</span> <br> <form action='http://localhost:5000/post/addVoteFromID' method='POST'> <button type='submit' name='id' value="
+                  + "</p> <form action='http://localhost:5000/post/addVoteFromID' method='POST'> <button style='width: 40px' type='submit' name='id' value="
                   + pitchid
-                  + "> upvote </button> </form></div> </div> </div>";
+                  + ">^</button> </form></div> </div> </div>";
         
     }; 
   document.getElementById("content-box").innerHTML = stuff;
@@ -44,11 +44,11 @@ document.getElementById("trending").onclick = function() {
           + proj
           + "</a></h3><p>"
           + desc
-          + "</div> <div class = 'container-right'> <h3> </h3> <span>Upvotes: " 
+          + "</div> <div class = 'container-right'> <h3> </h3> <p>Upvotes: " 
           + votes 
-          + "</span> <br> <form action='http://localhost:5000/post/addVoteFromID' method='POST'> <button type='submit' name='id' value="
+          + "</p> <br> <form action='http://localhost:5000/post/addVoteFromID' method='POST'> <button type='submit' name='id' value="
           + pitchid
-          + "> upvote </button> </form></div> </div> </div>";
+          + "> ^ </button> </form></div> </div> </div>";
       }; 
       document.getElementById("content-box").innerHTML = stuff;
     }
@@ -74,11 +74,11 @@ document.getElementById("recent").onclick = function() {
           + proj
           + "</a></h3><p>"
           + desc
-          + "</div> <div class = 'container-right'> <h3> </h3> <span>Upvotes: " 
+          + "</div> <div class = 'container-right'> <h3> </h3> <p>Upvotes: " 
           + votes 
-          + "</span> <br> <form action='http://localhost:5000/post/addVoteFromID' method='POST'> <button type='submit' name='id' value="
+          + "</p> <br> <form action='http://localhost:5000/post/addVoteFromID' method='POST'> <button type='submit' name='id' value="
           + pitchid
-          + "> upvote </button> </form></div> </div> </div>";
+          + "> ^ </button> </form></div> </div> </div>";
       }; 
       document.getElementById("content-box").innerHTML = stuff;
     }

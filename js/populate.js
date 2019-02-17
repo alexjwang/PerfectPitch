2b7b607c11de88f01d998a32d293b30f66b38425
K. Cameron Majidzadeh <katecm00@gmail.com>
2019-02-17 02:20:53 -0500

    Update populate.js

--- js/populate.js
+++ js/populate.js
@@ -7,21 +7,18 @@ xmlhttp.onreadystatechange = function() {
         var proj = element.name;
         var dept = element.dept;
         var votes = element.votes;
-        var isrc = 'img/item/'+dept+'.jpg';
-        var stuff = "<div class='row'><div class='col-md-4'><article class='aa-properties-item'><a href='#' class='aa-properties-item-img'><img src="
-                  + isrc
-                  + "alt='img'></a><div class='aa-tag for-sale'> "
+        var stuff = "<div class = 'col-md-12'><div class= 'project-display'><h3>"
+                  + proj
+                  + "</h3><p>"
+                  + dept
+                  +"</p><span>" 
                   + votes 
-                  +" </div><div class='aa-properties-item-content'><div class='aa-properties-info'><span>" 
-                  + dept 
-                  + "</span></div><div class='aa-properties-about'><h3><a href='#'>"
-                  + proj 
-                  + "</a></h3></div></div></div>";
-        console.log(stuff);
+                  + "</span></div></div>";
         document.getElementById("content-box").innerHTML += stuff;
     }; 
   }
 };
 xmlhttp.open("GET", "http://localhost:5000/get/getIDs?which=all", true);
 xmlhttp.send();
+
 

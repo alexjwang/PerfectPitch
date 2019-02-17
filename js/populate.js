7b05900586ecc9fb7d2d043d0b019db83d792ce6
Alex Wang <alexwang3150@gmail.com>
2019-02-17 03:30:29 -0500

    fixing upvote logic

--- js/populate.js
+++ js/populate.js
@@ -2,6 +2,7 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var myArr = JSON.parse(this.responseText) 
+    console.log(myArr);
     for(i = 0; i < myArr.length; i++) {
         var element = myArr[i];
         var proj = element.name;

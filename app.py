6d402117fda15f711c71576841a4d209d3735fc1
Alex Wang <alexwang3150@gmail.com>
2019-02-17 00:54:17 -0500

    tinkering with new boxes

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app = Flask(__name__,
 mysql = MySQL()
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'duckhacks2019'
+app.config['MYSQL_DATABASE_PASSWORD'] = '48650Pizz!'
+app.config['MYSQL_DATABASE_DB'] = 'duckhacks'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 mysql.init_app(app)
